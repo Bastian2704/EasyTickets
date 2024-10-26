39787bf17107240dc9955adb2c3259314b90a52a
Bastian2704 <sebastianabad2704@gmail.com>
2024-10-26 16:57:16 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EasyTickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
